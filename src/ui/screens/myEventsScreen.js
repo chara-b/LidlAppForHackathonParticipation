4443b0b4f09b01c73c6a1bf28d157fa0031df451
chara-b <chara.blgr@gmail.com>
2022-04-15 00:06:49 +0300

    events screen first positioning

--- src/ui/screens/myEventsScreen.js
+++ src/ui/screens/myEventsScreen.js
@@ -2,6 +2,7 @@ import { StyleSheet, Image, View, Text, StatusBar, Alert } from "react-native";
 import CHeader from "../components/molecules/CHeader/CHeader";
 import QuizList from "../components/organisms/quizList";
 import CButton from "../components/atoms/CButton/CButton";
+import { useState } from "react";
 const data = [
   {
     id: "1",
@@ -30,54 +31,71 @@ function ΜyEventsScreen(props) {
   const buttonLeftTitle = "Προσεχώς";
   const buttonRightTitle = "Οι εκδηλώσεις μου";
   const buttonCheckInTitle = "Check-in";
-
-  const buttonDisabled = false;
   const subTitle = "Εκδηλώσεις που θα πάω";
   const notificationTitle =
     "Παρακαλούμε προβείτε σε check-in την ημέρα και ώρα έναρξης της κάθε εκδήλωσης";
 
+  const onClickLeftButton = () => {};
+  const onClickRightButton = () => {};
   return (
     <>
       <CHeader title={headerTitle} timeOff={null} />
-      <CButton
-        title={buttonLeftTitle}
-        color={"white"}
-        fontWeight={"bold"}
-        onClick={Alert.alert("Προσεχώς")}
-        disabled={buttonDisabled}
-      />
-      <CButton
-        title={buttonRightTitle}
-        color={"white"}
-        fontWeight={"bold"}
-        onClick={Alert.alert("Οι εκδηλώσεις μου")}
-        disabled={buttonDisabled}
-      />
+      <View style={styles.buttonsContainer}>
+        <CButton
+          styles={styles.topButtons}
+          title={buttonLeftTitle}
+          color={"white"}
+          fontWeight={"bold"}
+          onClick={onClickLeftButton}
+          disabled={false}
+        />
+        <CButton
+          styles={styles.topButtons}
+          title={buttonRightTitle}
+          color={"white"}
+          fontWeight={"bold"}
+          onClick={onClickRightButton}
+          disabled={false}
+        />
+      </View>
       <Text style={styles.subTitle}>{subTitle}</Text>
-      <QuizList data={data} />
+
       <Text style={styles.notificationTitle}>{notificationTitle}</Text>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  quizBanner: {
+  buttonsContainer: {
     flex: 1,
-    width: "90%",
-    height: "100px",
-    marginTop: StatusBar.currentHeight || 0,
+    width: "100%",
+    height: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
   },
   subTitle: {
-    flex: 0.3,
+    flex: 7,
     padding: 10,
     color: "blue",
     fontWeight: "bold",
   },
   notificationTitle: {
-    flex: 0.7,
+    flex: 1,
     padding: 10,
     color: "black",
     fontWeight: "bold",
+  },
+  topButtons: {
+    flex: 1,
+    padding: 10,
+    width: "140px",
+    height: "140px",
+    backgroundColor: "white",
+    borderRadius: "10px",
+    textAlign: "center",
+    justifyContent: "center",
+    marginBottom: "10px",
+    border: "2px solid black",
   },
 });
 
