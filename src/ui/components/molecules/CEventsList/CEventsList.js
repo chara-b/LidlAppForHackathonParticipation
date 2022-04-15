2780b6c1dffab00b1a8282c31ed7ecaa5a0ac446
chara-b <chara.blgr@gmail.com>
2022-04-15 12:58:31 +0300

    CEventsList is done

--- src/ui/components/molecules/CEventsList/CEventsList.js
+++ src/ui/components/molecules/CEventsList/CEventsList.js
@@ -1,56 +1,80 @@
-import { FlatList } from "react-native";
+import { FlatList, View } from "react-native";
 import { useEffect, useState } from "react";
-import CListItem from "../../atoms/CListItem/CListItem";
 import { Foundation } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
-const CEventsList = ({ data }) => {
-  const [selectedItem, setSelectedItem] = useState({
-    id: 0,
-  });
+const CEventsList = ({
+  data,
+  buttonTitle,
+  buttonDisabled,
+  color,
+  fontWeight,
+  iconName,
+  iconSize,
+  iconColor,
+}) => {
+  //   const [selectedItem, setSelectedItem] = useState({
+  //     id: 0,
+  //   });
 
   const renderItem = ({ item }) => {
     return (
-      <View>
+      <View style={styles.container}>
         <Image source={require(item.icon)} />
-        <Text>{item.time}</Text>
-        <Text>{item.city}</Text>
-        <Text>{item.task}</Text>
-        <CButton
-          styles={styles.quizButton}
-          title={buttonTitle}
-          color={"white"}
-          fontWeight={"bold"}
-          onClick={onSubmit}
-          disabled={buttonDisabled}
-        />
-        <Foundation
-          name={iconName}
-          size={iconSize}
-          color={iconColor}
-          style={localStyles.icon}
-        />
-        <Foundation
-          name={iconName}
-          size={iconSize}
-          color={iconColor}
-          style={localStyles.icon}
-        />
+        <View style={styles.content}>
+          <Text>{item.time}</Text>
+          <Text>{item.city}</Text>
+          <Text>{item.task}</Text>
+          <CButton
+            styles={styles.button}
+            title={buttonTitle}
+            color={color}
+            fontWeight={fontWeight}
+            onClick={onSubmit}
+            disabled={buttonDisabled}
+          />
+        </View>
+        <View style={styles.rightVerticalButtons}>
+          <Ionicons name={iconName} size={iconSize} color={iconColor} />
+          <Ionicons name={iconName} size={iconSize} color={iconColor} />
+        </View>
       </View>
     );
   };
-  useEffect(() => {
-    // each time an item from the list is selected, this event is fired to get the changes from this component and send them to parent that we need the logic of enabling the button of the quiz if an answer is selected and then later check if it is the correct one...
-    handleSelectedItem(selectedItem); // this function is just a prop coming from the parent QuizList!
-  }, [selectedItem]);
+  //   useEffect(() => {
+  //     // each time an item from the list is selected, this event is fired to get the changes from this component and send them to parent that we need the logic of enabling the button of the quiz if an answer is selected and then later check if it is the correct one...
+  //     handleSelectedItem(selectedItem); // this function is just a prop coming from the parent QuizList!
+  //   }, [selectedItem]);
 
   return (
     <FlatList
       data={data}
       renderItem={renderItem}
       keyExtractor={(item) => item.id}
-      extraData={selectedItem}
+      // extraData={selectedItem}
     />
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  content: {
+    flex: 1,
+  },
+  rightVerticalButtons: {
+    flex: 1,
+  },
+  button: {
+    flex: 1,
+    padding: 20,
+    width: "180px",
+    backgroundColor: "rgb(26,115,232)",
+    borderRadius: "40px",
+    textAlign: "center",
+    justifyContent: "center",
+    marginBottom: "10px",
+  },
+});
 export default CEventsList;
---
+++ src/ui/components/organisms/eventsList.js
@@ -0,0 +1,75 @@
+import CList from "../molecules/CList/CList";
+import CButton from "../../components/atoms/CButton/CButton";
+import { Alert, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+
+const CEventsList = ({ data, isTimeOff }) => {
+  const buttonTitle = "ΥΠΟΒΟΛΗ";
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [correctAnswerItem, setCorrectAnswerItem] = useState(null);
+  //const [istimeoff, setIsTimeOff] = useState();
+
+  //   useEffect(() => {
+  //     setIsTimeOff(isTimeOff);
+  //   }, [isTimeOff]);
+
+  const handleSelectedItem = (item) => {
+    //if id !== 0 means the user selected something, so enable the button
+    if (item.id !== 0) {
+      //console.log(`Διάλεξες την ${item.id}η απάντηση`);
+      setButtonDisabled(false);
+      setCorrectAnswerItem(item);
+    }
+  };
+  const onSubmit = () => {
+    if (isTimeOff) {
+      onTimeOff();
+    } else {
+      if (correctAnswerItem.correctAnswer) {
+        Alert.alert(
+          `Η ${correctAnswerItem.id}η είναι η σωστή απάντηση! Συγχαρητήρια! 🥳`
+        );
+        console.log(
+          `Η ${correctAnswerItem.id}η είναι η σωστή απάντηση! Συγχαρητήρια! 🥳`
+        );
+      } else {
+        Alert.alert(
+          `Η ${correctAnswerItem.id}η δεν είναι η σωστή απάντηση... 😞`
+        );
+        console.log(
+          `Η ${correctAnswerItem.id}η δεν είναι η σωστή απάντηση... 😞`
+        );
+      }
+    }
+  };
+  const onTimeOff = () => {
+    Alert.alert("Time's off! You can't give an answer now! ⏲️");
+    console.log("Time's off! You can't give an answer now! ⏲️");
+  };
+  return (
+    <>
+      <CList data={data} handleSelectedItem={handleSelectedItem} />
+      <CButton
+        styles={styles.quizButton}
+        title={buttonTitle}
+        color={"white"}
+        fontWeight={"bold"}
+        onClick={onSubmit}
+        disabled={buttonDisabled}
+      />
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  quizButton: {
+    flex: 1,
+    padding: 20,
+    width: "180px",
+    backgroundColor: "rgb(26,115,232)",
+    borderRadius: "40px",
+    textAlign: "center",
+    justifyContent: "center",
+    marginBottom: "10px",
+  },
+});
+export default CEventsList;
