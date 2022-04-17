8f82d3bac8117e6683474553179287f2b9eafeca
chara-b <chara.blgr@gmail.com>
2022-04-17 04:16:06 +0300

    code input screen fixed

--- src/ui/screens/pinCodeScreen.js
+++ src/ui/screens/pinCodeScreen.js
@@ -1,68 +1,97 @@
 import { StyleSheet, Image, View, Text, ImageBackground } from "react-native";
 import CButton from "../components/atoms/CButton/CButton";
+import { Ionicons } from "@expo/vector-icons";
 
 function PinCodeScreen(props) {
   const description =
     "Παρακαλούμε καταχωρείστε το 4 ψήφιο κωδικό που λάβατε κατά τη διάρκεια του event";
-  const buttonTitle = "Επιστροφή";
+  const buttonTitle = "ΕΠΙΣΤΡΟΦΗ";
   const onSubmit = () => {};
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("../../assets/background.png")}
-        style={styles.pinCodeScreen}
+    <ImageBackground
+      source={require("../../assets/background.png")}
+      style={styles.pinCodeScreen}
+    >
+      <View
+        style={{
+          position: "absolute",
+          top: "5%",
+          left: "90%",
+          right: "5%",
+          bottom: "90%",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          textAlign: "center",
+        }}
       >
-        <CButton
-          styles={styles.closeButton}
-          onClick={onSubmit}
-          iconName={"close"}
-          iconSize={40}
-          iconColor={"black"}
+        <Ionicons
+          name="close"
+          size={24}
+          color="black"
+          style={styles.closeIcon}
         />
-        <View
-          style={{
-            position: "absolute",
-            top: "35%",
-            left: "35%",
-            right: "35%",
-            bottom: "35%",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "30px",
-            textAlign: "center",
-          }}
-        >
-          <Text style={{ flex: 1 }}>{description}</Text>
-        </View>
+      </View>
+      <View
+        style={{
+          position: "absolute",
+          top: "45%",
+          left: "0%",
+          right: "5%",
+          bottom: "0%",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        <Text style={styles.description}>{description}</Text>
+      </View>
+      <View
+        style={{
+          position: "absolute",
+          top: "60%",
+          left: "0%",
+          right: "50%",
+          bottom: "0%",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
         <CButton
           styles={styles.button}
           title={buttonTitle}
-          color={"white"}
+          color={"black"}
           fontWeight={"bold"}
           onClick={onSubmit}
         />
-      </ImageBackground>
-    </View>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-  },
   pinCodeScreen: {
     width: "100%",
     height: "100%",
   },
   button: {
+    flex: 2,
     padding: 20,
     width: "100%",
-    backgroundColor: "rgb(26,115,232)",
+    backgroundColor: "white",
     borderRadius: "40px",
     textAlign: "center",
-    justifyContent: "center",
+  },
+  description: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 17,
   },
 });
 
